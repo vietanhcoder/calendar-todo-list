6c179780e3ffd24d4df7e11c5b0024ed5846ccb1
Viet Anh Le <mc.vietanh@gmail.com>
2020-05-06 12:05:41 +0700

    Updates

---
+++ precache-manifest.120d38932f939cd0adb13072991998e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5566e643bab75ab5ed8903dfafee98d",
+    "url": "/github.com/calendar-todo-list/index.html"
+  },
+  {
+    "revision": "61e9d60ce75658d31ed4",
+    "url": "/github.com/calendar-todo-list/static/css/2.30e53678.chunk.css"
+  },
+  {
+    "revision": "5ff29009f078ddc1e649",
+    "url": "/github.com/calendar-todo-list/static/css/main.a9cf965a.chunk.css"
+  },
+  {
+    "revision": "61e9d60ce75658d31ed4",
+    "url": "/github.com/calendar-todo-list/static/js/2.8aab68fd.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/github.com/calendar-todo-list/static/js/2.8aab68fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff29009f078ddc1e649",
+    "url": "/github.com/calendar-todo-list/static/js/main.5c3d1723.chunk.js"
+  },
+  {
+    "revision": "39b3e5956af9663ff1db",
+    "url": "/github.com/calendar-todo-list/static/js/runtime-main.e3ca8ac4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/github.com/calendar-todo-list/precache-manifest.120d38932f939cd0adb13072991998e7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.com/calendar-todo-list/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
